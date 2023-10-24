9ede23c3d2a6c88ed55a49a1dcfb1880ad5b27c9
Facuusantillan <facusantillan1@gmail.com>
2023-10-24 20:19:18 -0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,8 @@ import DashBoard from './views/dashBoard/dashBoard';
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3001/'
-// axios.defaults.baseURL = 'https://calendar-production.up.railway.app/'
+// axios.defaults.baseURL = 'http://localhost:3001/'
+axios.defaults.baseURL = 'https://calendar-production.up.railway.app/'
 
 function App() {
 
